6c0cfb7b9d19389e14d30370289d4b99f84b0103
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2013-11-23 22:33:52 -0600

    Update to dbconnection

--- php/connectdb.php
+++ php/connectdb.php
@@ -3,7 +3,7 @@
 if (isset($con))
     $con->close();
 
-$con = new mysqli("localhost", "adminrdTYCLp", "qU65MCpSclmG", "php");
+$con = new mysqli("127.5.63.130", "adminrdTYCLp", "qU65MCpSclmG", "php");
 $_SESSION['connection'] = $con;
 
 //$con = new mysqli("mysql1.000webhost.com", "a9237023_root", "HBugi!123", "a9237023_hawala");
