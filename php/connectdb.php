1e3dc7611621e93e284b240f98d2983000ab2042
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2013-11-23 22:31:46 -0600

    Update to dbconnection

--- php/connectdb.php
+++ php/connectdb.php
@@ -3,7 +3,7 @@
 if (isset($con))
     $con->close();
 
-$con = new mysqli("mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/", "adminrdTYCLp", "qU65MCpSclmG", "php");
+$con = new mysqli("localhost", "adminrdTYCLp", "qU65MCpSclmG", "php");
 $_SESSION['connection'] = $con;
 
 //$con = new mysqli("mysql1.000webhost.com", "a9237023_root", "HBugi!123", "a9237023_hawala");
