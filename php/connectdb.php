4b441749e4f2bb2b06dd6b4e1718f86e0b55c0a9
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2013-12-21 10:34:38 -0600

    Update to change connectdb parameter

--- php/connectdb.php
+++ php/connectdb.php
@@ -1,9 +1,10 @@
 <?php
 
-if (!isset($con))
-//    $con = new mysqli("localhost", "root", "mysql123", "dtoe");
-  $con = new mysqli("localhost", "adminrdTYCLp", "qU65MCpSclmG", "php");
+if (isset($con))
+  $con->close();
 
+//$con = new mysqli("localhost", "root", "mysql123", "dtoe");
+  $con = new mysqli("localhost", "adminrdTYCLp", "qU65MCpSclmG", "php");
 $_SESSION['connection'] = $con;
 
 ?>
