1f4757cb4425bb042354557fc6db3c9f4d21f74c
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2013-12-21 22:43:26 -0600

    db connect

--- php/connectdb.php
+++ php/connectdb.php
@@ -4,7 +4,7 @@ if (isset($con))
   $con->close();
 
 //$con = new mysqli("localhost", "root", "mysql123", "dtoe");
-  $con = new mysqli("localhost", "root", "qU65MCpSclmG", "php");
+  $con = new mysqli("localhost", "adminrdTYCLp", "qU65MCpSclmG", "php");
 $_SESSION['connection'] = $con;
 
 ?>
